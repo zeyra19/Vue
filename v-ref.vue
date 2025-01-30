014a1d8ff6360e0f5401dd412966377fb1a066cd
zeyra19 <zehranurhindioglu@gmail.com>
2025-01-30 07:59:27 +0300

    change file type

---
+++ v-ref.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>
+      Using Vue with TypeScript
+      {{name}}
+      {{user}}
+  </h1>
+</template>
+
+<script setup lang="ts">
+import {ref, reactive} from "vue";
+
+interface User {
+  name: string,
+  age: number,
+  gender: "male" | "female"
+}
+
+  const name = ref("zehra")
+  name.value = "1"
+
+  const user = reactive<User>({
+    name: "Zehra",
+    age: 19,
+    gender: "female"
+  })
+</script>
+
+<style scoped>
+</style>
